0cb341390f7d96923a5b3cd79b11ce1e0f18f26a
Thomas Cook <tlncook85@gmail.com>
2022-05-28 11:00:54 -0600

    commit tree b9067c97ee4fad6ab0e5cff123fa0bf3890dc345

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+// // Add your code here
+function submitData(name, email) {
+ 
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({ name, email })
+    })
+    .then((response) => response.json())
+    .then((resData) => {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = resData.id;
+        document.body.append(paragraph);
+    })
+    .catch((error) => {
+        const errorMessage = document.createElement('p')
+        errorMessage.textContent = error.message;
+        document.body.append(errorMessage);
+    });
+}
